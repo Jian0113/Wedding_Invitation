a5a83ae7ab7ba696a229979dd7eab2bd4f59b2b1
unknown <eunbin1563@gmail.com>
2026-02-02 17:36:24 +0900

    ICO 위치 변경

--- pages/index.js
+++ pages/index.js
@@ -27,11 +27,11 @@ export default function Home() {
       >
         <main className={styles.main}>
           <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js logo"
-            width={100}
-            height={20}
+            className={styles.heroLogo}
+            src="/Wedding_Logo.ico"
+            alt="Wedding logo"
+            width={200}
+            height={200}
             priority
           />
           <div className={styles.intro}>
